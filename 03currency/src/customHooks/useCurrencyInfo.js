e85e73e21abbed11104338e592b8fb2470c88246
zeeshan shaikh <skzeeshan10011@gmail.com>
2024-07-06 15:49:44 +0530

    new project added

---
+++ 03currency/src/customHooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+
+    const [data, setdata] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((response) => response.json())
+        .then((reponse) => setdata(reponse[currency]))
+    },[currency])
+
+    return data
+}
+
+
+export default useCurrencyInfo;
